0f680bbe4c995dd2012f5d07650e55d24cbdea43
softwareByAndi <softwareByAndi@gmail.com>
2024-11-18 09:13:57 -0700

    fix comments in section macros

--- lib/snippets/io_macros.h
+++ lib/snippets/io_macros.h
@@ -2,7 +2,7 @@
 #include "ascii_colors.h"
 
 #define LOG_SUCCESS std::cout << GREEN << "  SUCCESS" << RESET << std::endl << std::endl
-#define LOG_FAILURE std::cerr << "  FAILURE" << std::endl << std::endl
+#define LOG_FAILURE std::cerr << RED << "  FAILURE" << std::endl << RESET << std::endl
 #define FAIL(x) LOG_FAILURE; throw std::runtime_error(x)
 #define print(x) std::cout << x << std::endl
 #define list(x) std::cout << "  - " << x << std::endl
@@ -13,5 +13,5 @@
 #define NEWLINE std::cout << std::endl
 #define DNL std::endl << std::endl;
 
-#define SECTION(name) print(name); // <editor-fold desc="name">
-#define END_SECTION // </editor-fold>
+#define SECTION(name) print(name); /*<editor-fold desc="name">*/
+#define END_SECTION /*</editor-fold>*/
