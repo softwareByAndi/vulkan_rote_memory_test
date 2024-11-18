841e26315844c0bbaa1a8376dd5407f3924e1fd4
softwareByAndi <softwareByAndi@gmail.com>
2024-11-18 09:13:28 -0700

    add more terminal colors

--- lib/snippets/ascii_colors.h
+++ lib/snippets/ascii_colors.h
@@ -1,5 +1,12 @@
-#define RESET "\033[0m"
-#define GRAY "\033[90m"
-#define YELLOW "\033[33m"
-#define GREEN "\033[32m"
-#define BLUE "\033[34m"
+
+#define RESET   "\033[0m"      // Resets to default terminal color
+#define GRAY    "\033[90m"     // Subdued gray, for the things you don't want to yell about
+#define BLUE    "\033[34m"     // Blue, because nothing says “trustworthy” like blue
+#define GREEN   "\033[32m"     // A delightful shade of “it works” green
+#define YELLOW  "\033[33m"     // Eye-searing yellow, for those subtle warnings
+#define RED     "\033[31m"     // Fiery red, perfect for errors
+
+#define BLACK   "\033[30m"     // Dreadfully dark, as a void
+#define MAGENTA "\033[35m"     // A rebellious magenta, for when you feel bold
+#define CYAN    "\033[36m"     // Cool cyan, for info that doesn’t feel dangerous
+#define WHITE   "\033[37m"     // Bright white, for supreme clarity
