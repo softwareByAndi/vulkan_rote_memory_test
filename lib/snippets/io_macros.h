e03b4388ba4a0836872770ff0cd7f983d66a3fc3
softwareByAndi <softwareByAndi@gmail.com>
2024-12-15 14:40:54 -0700

    update io macros

--- lib/snippets/io_macros.h
+++ lib/snippets/io_macros.h
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "ascii_colors.h"
 
@@ -7,11 +8,27 @@
 #define print(x) std::cout << x << std::endl
 #define list(x) std::cout << "  - " << x << std::endl
 #define list_blue(x) std::cout << BLUE << "  - " << x << RESET << std::endl
+#define list_red(x) std::cout << RED << "  - " << x << RESET << std::endl
 #define list_gray(x) std::cout << GRAY << "  - " << x << RESET << std::endl
 #define label(x, y) std::cout << "  - " << x << " : " << std::boolalpha << y << std::endl
 #define label_blue(x, y) std::cout << BLUE << "  - " << x << " : " << std::boolalpha << y << RESET << std::endl
+#define label_red(x, y) std::cout << RED << "  - " << x << " : " << std::boolalpha << y << RESET << std::endl
 #define NEWLINE std::cout << std::endl
 #define DNL std::endl << std::endl;
 
 #define SECTION(name) print(name); /*<editor-fold desc="name">*/
 #define END_SECTION /*</editor-fold>*/
+
+
+namespace IO {
+void print_vec(std::vector<const char *> v, const char * color) {
+  for (const auto & s : v) {
+    std::cout << color << "- " << s << RESET << std::endl;
+  }
+}
+
+void print_vec(std::vector<const char *> v) {
+  print_vec(v, GRAY);
+}
+
+}
